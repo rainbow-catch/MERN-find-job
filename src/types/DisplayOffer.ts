1293d3cbbe2c8729e4c78e0f29184a38b6306bfb
milosbayar <milosbayar114@gmail.com>
2023-01-20 20:01:58 +0100

    created offer and displayOffer tpyes

---
+++ src/types/DisplayOffer.ts
@@ -0,0 +1,19 @@
+export interface DisplayOffer {
+  id_: string;
+  company_name: string;
+  days_ago: string;
+  contract_types: string;
+  country: string;
+  ad_content: string;
+  job_type: string;
+  seniority: string;
+  technology_1: string;
+  technology_2: string;
+  technology_3: string;
+  salary: string;
+  description: string;
+  about_us: string;
+  logo: string;
+  isDescriptionVisible: boolean;
+  frontendId: number;
+}
---
+++ src/types/Offer.ts
@@ -0,0 +1,16 @@
+export interface Offer {
+  company_name: string;
+  days_ago: string;
+  contract_types: string;
+  country: string;
+  ad_content: string;
+  job_type: string;
+  seniority: string;
+  technology_1: string;
+  technology_2: string;
+  technology_3: string;
+  salary: string;
+  description: string;
+  about_us: string;
+  logo: string;
+}
