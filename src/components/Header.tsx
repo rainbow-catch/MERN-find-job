0ea9bbe292dc044e2f866bca1d581a8e7463a15b
milosbayar <milosbayar114@gmail.com>
2023-02-10 19:19:34 +0100

    completely redesigned header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import "../styles/style.css";
-import "../styles/Header.css"
+import "../styles/Header.css";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 import AddOfferModal from "./AddOfferModal";
@@ -21,97 +21,101 @@ function Header({
   const loggedCompany: string = loggedUser.company_name;
   const loggedCompanyLogo: string = loggedUser.logo;
   return (
-    <div id="upperNavbar">
-      <img
-        className="jobSearchLogo"
-        src="https://raw.githubusercontent.com/ajgoras/job-search-mern/main/csv/images/job-search-low-resolution-logo-color-on-transparent-background.png"
-        alt="logo"
-      ></img>
-      {loggedUser.email === "" ? (
-        <span></span>
-      ) : (
-        <div id="loggedUserInfo">
-          <p className="purpleColor">Logged as {loggedUser.email}</p>
-        </div>
-      )}
-
-      <div></div>
-      <div className="headerButtonsDiv">
+    <div>
+      <div id="upperNavbar">
+        <img
+          className="jobSearchLogo"
+          src="https://raw.githubusercontent.com/ajgoras/job-search-mern/main/csv/images/job-search-logo-white.png"
+          alt="logo"
+        ></img>
         {loggedUser.email === "" ? (
           <span></span>
         ) : (
-          <div className="headerButtonsAfterLoginDiv">
-            <Button
-              className="headerButton"
-              onClick={() => setShowShowApplicationsModal(true)}
-            >
-              Show Applications
-            </Button>
-            <Button
-              className="headerButton"
-              onClick={() => setShowAddOfferModal(true)}
-            >
-              Add Offer
-            </Button>
+          <div id="loggedUserInfo">
+            <p className="purpleColor">Logged as {loggedUser.email}</p>
           </div>
         )}
-        {loggedUser.email === "" ? (
-          <Button
-            className="headerButton"
-            id="loginButton"
-            onClick={() => setShowLoginModal(true)}
-          >
-            Login
-          </Button>
-        ) : (
-          <Button
-            className="headerButton"
-            id="loginButton"
-            onClick={() => {
-              window.location.reload();
-            }}
-          >
-            Logout
-          </Button>
-        )}
-        {loggedUser.email === "" ? (
-          <Button
-            className="headerButton"
-            id="registerButton"
-            onClick={() => setShowRegisterModal(true)}
-          >
-            Register
-          </Button>
-        ) : (
-          <span></span>
-        )}
-      </div>
 
-      <LoginModal
-        show={showLoginModal}
-        onHide={() => setShowLoginModal(false)}
-        setloggeduser={setLoggedUser}
-        setloggedasadmin={setLoggedAsAdmin}
-      ></LoginModal>
-      <RegisterModal
-        show={showRegisterModal}
-        onHide={() => setShowRegisterModal(false)}
-        setloggeduser={setLoggedUser}
-        setloggedasadmin={setLoggedAsAdmin}
-      ></RegisterModal>
-      <AddOfferModal
-        loggedcompany={loggedCompany}
-        loggedcompanylogo={loggedCompanyLogo}
-        loggedasadmin={loggedAsAdmin}
-        show={showAddOfferModal}
-        onHide={() => setShowAddOfferModal(false)}
-      ></AddOfferModal>
-      <ShowApplicationsModal
-        loggedcompany={loggedCompany}
-        loggedasadmin={loggedAsAdmin}
-        show={showShowApplicationsModal}
-        onHide={() => setShowShowApplicationsModal(false)}
-      ></ShowApplicationsModal>
+        <div></div>
+        <div className="headerButtonsDiv">
+          {loggedUser.email === "" ? (
+            <span></span>
+          ) : (
+            <div className="headerButtonsAfterLoginDiv">
+              <Button
+                className="headerButton"
+                onClick={() => setShowShowApplicationsModal(true)}
+              >
+                Show Applications
+              </Button>
+              <Button
+                className="headerButton"
+                onClick={() => setShowAddOfferModal(true)}
+              >
+                Add Offer
+              </Button>
+            </div>
+          )}
+          {loggedUser.email === "" ? (
+            <Button
+              className="headerButton"
+              id="loginButton"
+              onClick={() => setShowLoginModal(true)}
+            >
+              Login
+            </Button>
+          ) : (
+            <Button
+              className="headerButton"
+              id="loginButton"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              Logout
+            </Button>
+          )}
+          {loggedUser.email === "" ? (
+            <Button
+              className="headerButton"
+              id="registerButton"
+              onClick={() => setShowRegisterModal(true)}
+            >
+              Register
+            </Button>
+          ) : (
+            <span></span>
+          )}
+        </div>
+      </div>
+      <div id="headerTextDiv">
+        <h2>Find a job in IT</h2>
+      </div>
+        <LoginModal
+          show={showLoginModal}
+          onHide={() => setShowLoginModal(false)}
+          setloggeduser={setLoggedUser}
+          setloggedasadmin={setLoggedAsAdmin}
+        ></LoginModal>
+        <RegisterModal
+          show={showRegisterModal}
+          onHide={() => setShowRegisterModal(false)}
+          setloggeduser={setLoggedUser}
+          setloggedasadmin={setLoggedAsAdmin}
+        ></RegisterModal>
+        <AddOfferModal
+          loggedcompany={loggedCompany}
+          loggedcompanylogo={loggedCompanyLogo}
+          loggedasadmin={loggedAsAdmin}
+          show={showAddOfferModal}
+          onHide={() => setShowAddOfferModal(false)}
+        ></AddOfferModal>
+        <ShowApplicationsModal
+          loggedcompany={loggedCompany}
+          loggedasadmin={loggedAsAdmin}
+          show={showShowApplicationsModal}
+          onHide={() => setShowShowApplicationsModal(false)}
+        ></ShowApplicationsModal>
     </div>
   );
 }
