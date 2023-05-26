3735ab1b45430a42ece25b8c6a88c5150da01291
milosbayar <milosbayar114@gmail.com>
2023-05-26 21:06:43 +0200

    fixed eslint warnings in checkFormValidity

--- src/functions/checkFormValidity.ts
+++ src/functions/checkFormValidity.ts
@@ -9,18 +9,18 @@ export const checkFormValidity = (formElements: any) => {
 
   vals.forEach((val, index) => {
     if (keys[index] === "email") {
-      if (val.match(/[A-Za-z0-9]+@[A-Za-z0-9]+/i) && val != null && val != "") {
+      if (val.match(/[A-Za-z0-9]+@[A-Za-z0-9]+/i) && val !== null && val !== "") {
         counter++;
       }
     } else if (keys[index] === "logo") {
       if (
         val.match(/[A-Za-z]+:\/\/[A-Za-z0-9]+\.[A-Za-z]+/i) &&
-        val != null &&
-        val != ""
+        val !== null &&
+        val !== ""
       ) {
         counter++;
       }
-    } else if (val != null && val != "") {
+    } else if (val !== null && val !== "") {
       counter++;
     }
   });
