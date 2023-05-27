a665d4d392b7d52ec0f83d8f8cdb31f31a0e7d73
milosbayar <milosbayar114@gmail.com>
2023-05-27 17:40:42 +0200

    implemented automatic checking serverUrl

--- src/axiosUrls/axiosUrls.ts
+++ src/axiosUrls/axiosUrls.ts
@@ -1,12 +1,21 @@
+let serverUrl = "https://jobsearchmern.onrender.com";
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  serverUrl = "http://localhost:8888";
+} else {
+  serverUrl = "https://jobsearchmern.onrender.com";
+}
+
 export const axiosUrls = {
   cloudinaryApi: "https://api.cloudinary.com/v1_1/dyqgdjrr1/raw/upload",
-  createOfferUrl: "http://localhost:8888/create",
-  sendApplicationUrl: "http://localhost:8888/sendApplication",
-  getOffersUrl: "http://localhost:8888/sendToFront",
-  loginUrl: "http://localhost:8888/login",
-  registerUrl: "http://localhost:8888/register",
+
+  createOfferUrl: `${serverUrl}/create`,
+  sendApplicationUrl: `${serverUrl}/sendApplication`,
+  getOffersUrl: `${serverUrl}/sendToFront`,
+  loginUrl: `${serverUrl}/login`,
+  registerUrl: `${serverUrl}/register`,
   removeOfferUrl: (id: number) => {
-    return `http://localhost:8888/removeOffer/${id}`;
+    return `${serverUrl}/removeOffer/${id}`;
   },
-  getApplicationsUrl: "http://localhost:8888/getApplications",
+  getApplicationsUrl: `${serverUrl}/getApplications`,
 };
