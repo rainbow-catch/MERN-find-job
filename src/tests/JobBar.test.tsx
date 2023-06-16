eeafd023ba6ebb4c5856b766c934815027f7fc9a
milosbayar <milosbayar114@gmail.com>
2023-06-16 15:20:28 +0200

    JobBar test now checks that all offers display

--- src/tests/JobBar.test.tsx
+++ src/tests/JobBar.test.tsx
@@ -4,12 +4,35 @@ import JobBar from "../components/JobBar";
 import { ContextsProvider } from "../contexts/Contexts";
 import "@testing-library/jest-dom";
 
-test("renders job offers", async () => {
+test("should render default job offers", async () => {
   render(
     <ContextsProvider>
       <JobBar searchTags={[]} searchText=""></JobBar>
     </ContextsProvider>
   );
-  const jobBarElement = screen.getByTitle("jobBarContainer");
-  expect(jobBarElement).toBeInTheDocument();
+  const jobsCompanies = [
+    "Sutton Builders",
+    "Yorke Cycles",
+    "Diamond Photography",
+    "Lear Finishing Ltd",
+    "Dimbleby Refrigeration",
+    "O'Grady Exhibitions",
+    "Fine Hire Cars Ltd",
+    "Expert Technologies",
+    "Silver Fabrics",
+    "Anderton Lab Equipment Ltd",
+    "Ingram Automotive Ltd",
+    "Ingeman Workshops",
+    "Saddlemore Office Furniture",
+    "Leonard Kitchens Ltd",
+    "Riverside Transport",
+  ];
+
+  const jobBarContainer = screen.getByTitle("jobBarContainer");
+  expect(jobBarContainer).toBeInTheDocument();
+
+  for (const el of jobsCompanies) {
+    const jobBarElement = await screen.findByText(el);
+    expect(jobBarElement).toBeInTheDocument();
+  }
 });
