7c241b9cd3f9babf52927f0865c858d7746173f2
milosbayar <milosbayar114@gmail.com>
2023-06-27 20:38:30 +0200

    created browser router

---
+++ src/routes/router.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import Header from "../components/Header";
+import SearchBarWithJobBar from "../components/SearchBarWithJobBar";
+import ShowApplicationsModal from "../components/ShowApplicationsModal";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div id="Index">
+        <div id="upperBackground">
+          <Header></Header>
+        </div>
+        <SearchBarWithJobBar></SearchBarWithJobBar>
+      </div>
+    ),
+  },
+  {
+    path: "applications",
+    element: (
+      <>
+        <div id="Index">
+          <div id="upperBackground">
+            <Header></Header>
+          </div>
+          <SearchBarWithJobBar></SearchBarWithJobBar>
+        </div>
+        <ShowApplicationsModal
+          show={true}
+        ></ShowApplicationsModal>
+      </>
+    ),
+  },
+]);
